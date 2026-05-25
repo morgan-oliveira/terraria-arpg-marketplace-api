ee57ac30d67da4d13e3ece3f4a3ba44dbfbedf36
Morgan Oliveira <morgan.matprof@gmail.com>
2026-05-25 13:51:56 -0300

    fix: marketplace without modauth injection

--- src/modules/items/items.module.ts
+++ src/modules/items/items.module.ts
@@ -3,10 +3,12 @@ import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { ModAuthModule } from '../../common/services/mod-auth.module';
+import { ModAuthService } from 'src/common/services/mod-auth.service';
+import { MarketplaceService } from '../marketplace/marketplace.service';
 
 @Module({
   imports: [ModAuthModule],
   controllers: [ItemsController],
-  providers: [ItemsService, PrismaService],
+  providers: [ItemsService, PrismaService, ModAuthService, MarketplaceService],
 })
 export class ItemsModule {}
--- src/modules/marketplace/marketplace.module.ts
+++ src/modules/marketplace/marketplace.module.ts
@@ -3,9 +3,10 @@ import { MarketplaceService } from './marketplace.service';
 import { MarketplaceController } from './marketplace.controller';
 import { ItemsService } from '../items/items.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { ModAuthService } from 'src/common/services/mod-auth.service';
 
 @Module({
   controllers: [MarketplaceController],
-  providers: [MarketplaceService, ItemsService, PrismaService],
+  providers: [MarketplaceService, ItemsService, PrismaService, ModAuthService],
 })
 export class MarketplaceModule {}
